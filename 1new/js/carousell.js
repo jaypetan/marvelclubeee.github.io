46b60a661e479558106ec595930b168cdc72d033
Jun Peng <jptann01@gmail.com>
2025-03-09 15:08:08 +0200

    update

--- 1new/js/carousell.js
+++ 1new/js/carousell.js
@@ -1,8 +1,9 @@
-let slideIndex = [1,1];
-let slideId = ["events", "projects"]
-let indexId = ["index-events", "index-projects"]
+let slideIndex = [1, 1, 1]; // stored data to represent the slide index of the respective slide
+let slideId = ["events", "projects", "committee"]
+let indexId = ["index-events", "index-projects", "index-committee"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
@@ -10,14 +11,14 @@ function plusSlides(n, no) {
 
 function showSlides(n, no) {
   let i;
-  let images = document.getElementsByClassName(slideId[no]);
+  let slides = document.getElementsByClassName(slideId[no]);
   let index = document.getElementsByClassName(indexId[no]);
-  if (n > images.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = images.length}
-  for (i = 0; i < images.length; i++) {
-     images[i].style.display = "none";  
+  if (n > slides.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = slides.length}
+  for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";  
      index[i].classList.remove("active");
   }
-  images[slideIndex[no]-1].style.display = "block"; 
+  slides[slideIndex[no]-1].style.display = "flex"; // note slideIndex[no]-1 the correct index of the slide
   index[slideIndex[no]-1].classList.add("active");
 }
